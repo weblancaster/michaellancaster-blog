dcecc083ffc987916c9bf61ac48214557e058c9a
Michael Lancaster <michaellancaster@Michaels-MacBook-Air.local>
2018-06-26 02:49:11 -0700

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/michaellancaster-blog/app-0183c7663a6bc725db92.js","9b20c837552086276c13bc1391dd10dc"],["/michaellancaster-blog/app-03ba97b23f6c1410b24c.js","4a5eff679c0889ff1ecfadd262bebb45"],["/michaellancaster-blog/app-42a12a1e3fa3f1973020.js","7e1eebe958ef40306f712c1e1a827b7d"],["/michaellancaster-blog/app-7053ff3940e7c4f80114.js","5656edf82234b091e0dcf581ce9b2a15"],["/michaellancaster-blog/commons-634c5c5d9297f6ebe288.js","b225eb8b7e4de3d4b8460f69e69e7618"],["/michaellancaster-blog/index.html","52beafc44904e992767f910eaf027c9a"],["/michaellancaster-blog/offline-plugin-app-shell-fallback/index.html","c1a309f81b1b99c02715262fcc283591"]];
+var precacheConfig = [["/michaellancaster-blog/app-0183c7663a6bc725db92.js","9b20c837552086276c13bc1391dd10dc"],["/michaellancaster-blog/app-03ba97b23f6c1410b24c.js","4a5eff679c0889ff1ecfadd262bebb45"],["/michaellancaster-blog/app-42a12a1e3fa3f1973020.js","7e1eebe958ef40306f712c1e1a827b7d"],["/michaellancaster-blog/app-4543731bd9dace3d8023.js","b6757fa6fb8fa7574542a1c27e6fb82e"],["/michaellancaster-blog/app-7053ff3940e7c4f80114.js","5656edf82234b091e0dcf581ce9b2a15"],["/michaellancaster-blog/commons-634c5c5d9297f6ebe288.js","b225eb8b7e4de3d4b8460f69e69e7618"],["/michaellancaster-blog/index.html","c47ce29d17ae80949ba232459735fd21"],["/michaellancaster-blog/offline-plugin-app-shell-fallback/index.html","c542e5c9037bb69f49d490612ff39eb8"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
